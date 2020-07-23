36e0921e3c45fbc92fa818413edd0650590ce8b6
Shivam Rastogi <68611326+shivamrastogi1152@users.noreply.github.com>
2020-07-24 02:21:32 +0530

    Update mergesort.js

--- js/mergesort.js
+++ js/mergesort.js
@@ -1,84 +1,86 @@
-async function sleep(ms)
-{
-    return new Promise(resolve => setTimeout(resolve,ms));
-}
+// async function sleep(ms)
+// {
+//     return new Promise(resolve => setTimeout(resolve,ms));
+// }
 
-let arr = [];
-let w = 5;
+// let arr = [];
+// let w = 5;
 
-var curr_size=1;
-var left_start = 0;
+// var curr_size=1;
+// var left_start = 0;
 
-function setup(){
-    createCanvas(1000,600);
-    var Canvas = document.getElementById("defaultCanvas0");
-    Canvas.style.display = 'block';
-    Canvas.style.margin = '0 auto';
-    Canvas.style.border = '5px solid black';
-    Canvas.style.marginTop = '100px';
+// function setup(){
+//     createCanvas(1000,600);
+//     var Canvas = document.getElementById("defaultCanvas0");
+//     Canvas.style.display = 'block';
+//     Canvas.style.margin = '0 auto';
+//     Canvas.style.border = '5px solid black';
+//     Canvas.style.marginTop = '100px';
 
-    arr = new Array(floor(width/w));
+//     arr = new Array(floor(width/w));
 
-    for(let i=0;i<arr.length;i++){
-       arr[i]= random(height);
-    }
-    frameRate(30);
+//     for(let i=0;i<arr.length;i++){
+//        arr[i]= random(height);
+//     }
+//     frameRate(30);
 
-    // mergeSort(arr,0,arr.length-1);
-}
+//     mergeSort(arr,0,arr.length-1);
+// }
 
-function draw(){
+// async function draw(){
 
-    background(51);
-    for(let i=0;i<arr.length;i++){
-        rect(i*w,height-arr[i],w,arr[i]);
-    }
+//     background(51);
+//     for(let i=0;i<arr.length;i++){
+//         rect(i*w,height-arr[i],w,arr[i]);
+//     }
 
-    if(curr_size<=arr.length)
-    {
-        for(left_start=0;left_start<arr.length-1;left_start+= 2*curr_size){
+//     // if(curr_size<=arr.length)
+//     // {
+//     //     for(left_start=0;left_start<arr.length-1;left_start+= 2*curr_size){
 
-            let mid = min(left_start+curr_size-1,arr.length-1);
-            var right_end = min(left_start+2*curr_size-1,arr.length-1);
+//     //         let mid = min(left_start+curr_size-1,arr.length-1);
+//     //         var right_end = min(left_start+2*curr_size-1,arr.length-1);
 
-            merge(arr,left_start,mid,right_end);
-        }
-        await sleep(1000);
-        curr_size*=2;
-    }
-    else{
-        console.log("Sorted");
-        noLoop();
-    }
+//     //         merge(arr,left_start,mid,right_end);
+//     //         await sleep(1);
+//     //     }
+        
+//     //     curr_size*=2;
+//     // }
+//     // else{
+//     //     console.log("Sorted");
+//     //     noLoop();
+//     // }
 
-}
+// }
 
-function merge(arr,l,m,r)
-{   
-    console.log("Merge Called");
-    let i=0;
-    let j=m+1;
-    let k=0;
+// function merge(arr,l,m,r)
+// {   
+//     console.log("Merge Called");
+//     var i=l;
+//     var j=m+1;
+//     var k=0;
+//     var size = r - l + 1;
 
-    let temp = new Array(r-l+1);
+//     var temp = new Array(size);
 
-    while(i<=m && j<=r){
-        if(arr[i]<arr[j]){
-            temp[k++]=arr[i++];
-        }
-        else temp[k++]=arr[j++];
-    }
+//     while(i<=m && j<=r){
+//         if(arr[i]<arr[j]){
+//             temp[k++]=arr[i++];
+//         }
+//         else temp[k++]=arr[j++];
+//     }
 
-    while(i<=m) temp[k++]=arr[i++];
-    while(j<=r) temp[k++]=arr[j++];
+//     while(i<=m) temp[k++]=arr[i++];
+//     while(j<=r) temp[k++]=arr[j++];
 
-    i=l;
-    k=0;
-    for(i=l;i<=r;i++)
-    {
-        arr[i]=temp[k++];
-    }
-}
+//     i=l;
+//     k=0;
+//     for(i=l;i<=r;i++)
+//     {
+//         arr[i]=temp[k++];
+//     }
+// }
 
 // async function mergeSort(arr,l,r)
 // {
